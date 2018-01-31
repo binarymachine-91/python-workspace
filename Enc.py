0bf3aa4abe50f8fa5b319f1beb52e14fc7b513ee
vignesh-geek <vigne.sh@live.com>
2018-01-31 16:57:27 +0530

    !!

--- Enc.py
+++ Enc.py
@@ -6,6 +6,7 @@ op_chk = 0
 fin_dec = 0
 inp_val2 = 0
 dec_loop = 0
+ex = 0
 #print('***************** Simple Encrpter and Decrypter Program **************')
 #print('   $$$$$$$$$$$$$$$$$ Logic R.Sankar  Code : G.vignesh $$$$$$$$$       ')
 #print('                 ~~~~~~~~ Verson 1.0 ~~~~~~~~                         ')
@@ -55,33 +56,47 @@ if op == 1: #Condition Check
             else:
                 break
         inp_val.append(val)
-
+    def encrypt_no(inp_val):
     # Encryption Begins Here
-    for j in range(len(inp_val)):
-        if j == 0:
-            decrp_val = int(inp_val[j]) * 256
-            fin_dec = decrp_val + int(inp_val[j+1])
-        elif j == len(inp_val) - 1:
-            break
-        else:
-            decrp_val = int(fin_dec) * 256
-            fin_dec = decrp_val + int(inp_val[j+1])
-    print('Encrypted Value : {}'.format(fin_dec))
-    ex = input('Press Enter to Exit')
+        for j in range(len(inp_val)):
+            if j == 0:
+                decrp_val = int(inp_val[j]) * 256
+                fin_dec = decrp_val + int(inp_val[j+1])
+            elif j == len(inp_val) - 1:
+                break
+            else:
+                decrp_val = int(fin_dec) * 256
+                fin_dec = decrp_val + int(inp_val[j+1])
+        print('Encrypted Value : {}'.format(fin_dec))
+        ex = input('Press Enter to Exit')
+    encrypt_no(inp_val)
+    #print(out)
+
 # Decryption Block
 elif op == 2:
     print('Decryption Block')
     cipher_txt = int(input('Enter the Encrypted Value : '))
-    while dec_loop == 0:
-        qut,rem = divmod(cipher_txt,256)
-        out_val.append(rem)
-        cipher_txt = qut
-        if qut < 255:
-            out_val.append(qut)
-            dec_loop = 1
-    out_val.reverse()
-    print('Decrypted Input : {}'.format(out_val))
-    ex = input('Press Enter to Exit')
+
+    #This is a function to decrypt a no
+    def decrypt_no(cipher_txt):
+        dec_loop = 0
+        out_val.clear()
+    #Decrption Begins here
+        while dec_loop == 0:
+            qut,rem = divmod(cipher_txt,256)
+            out_val.append(rem)
+            cipher_txt = qut
+            if qut < 255:
+                out_val.append(qut)
+                dec_loop = 1
+        out_val.reverse()
+        print('Decrypted Input : {}'.format(out_val))
+        ex = int(input('Press 1 Continue'))
+
+    decrypt_no(cipher_txt)
+con = int(input('Do u Want to Continue press 1'))
+while dec_con == 0:
+    pass
 
 # Exit from Program
 elif op == 3:
